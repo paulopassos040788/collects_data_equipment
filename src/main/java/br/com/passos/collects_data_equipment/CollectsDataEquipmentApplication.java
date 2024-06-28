c4f065b31901f8e002c0a7ed1600b285d12aad5c
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-06-27 22:51:45 -0300

    inicio do projeto

---
+++ src/main/java/br/com/passos/collects_data_equipment/CollectsDataEquipmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.passos.collects_data_equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectsDataEquipmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectsDataEquipmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/passos/collects_data_equipment/CollectsDataEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passos.collects_data_equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectsDataEquipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
