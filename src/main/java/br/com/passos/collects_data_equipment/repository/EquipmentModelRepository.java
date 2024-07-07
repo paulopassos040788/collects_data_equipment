35f0fbf738b1721e005005d95446c704f8449951
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-07-07 17:25:42 -0300

    criacao do repository EquipmentModelRepository

---
+++ src/main/java/br/com/passos/collects_data_equipment/repository/EquipmentModelRepository.java
@@ -0,0 +1,7 @@
+package br.com.passos.collects_data_equipment.repository;
+
+import br.com.passos.collects_data_equipment.models.EquipmentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentModelRepository extends JpaRepository<EquipmentModel, Long> {
+}
